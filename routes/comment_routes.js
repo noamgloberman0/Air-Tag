a815e2d28b47e27791b30348e74ed278dc941c46
noamgloberman0 <noam.globerman@placer.ai>
2024-11-23 14:26:49 +0200

    Feat: Update comments

--- routes/comment_routes.js
+++ routes/comment_routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const comment_controller = require('../controllers/comment_controller');
 
 router.post('/', comment_controller.create_comment);
+router.put('/', comment_controller.update_comment);
 
 module.exports = router;
